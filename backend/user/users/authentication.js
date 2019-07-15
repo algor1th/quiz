82207befc0ce60044ee37647ddb24ce26746f8e4
Andreas Hanuj <hanuja2@gmail.com>
2019-07-15 14:21:55 +0200

    added paths for authentication to gameserver

--- backend/user/users/authentication.js
+++ backend/user/users/authentication.js
@@ -6,19 +6,50 @@ const joi = require('@hapi/joi');
 const rand = require('csprng');
 
 const schemaUserID = {
-    id: joi.number().required()
+    userID: joi.number().required()
 }
 
 module.exports = function(app){
     
     //create a token for a user
     app.post('/api/authentication', async (req, res) => {
- 
-            console.log(rand(128, 16));
+
+        const sanitizeResult = joi.validate(req.body, schemaUserID);
+        if(sanitizeResult.error){
+            res.status(400).send(sanitizeResult.error.details[0].message);
+            return;
+        }
+
+        const user = await maria.query('SELECT * FROM users WHERE id = ?', [req.body.userID]);
+        if(user.length === 0){
+            res.status(404).send(`user with id ${req.body.userID} not found`);
+            return;
+        }
+        console.log(user[0]["token"])
+        if(user[0]["token"] !== null){
+            res.status(404).send(`user with id ${req.body.userID} already authenticated`);
+            return;
+        }
+
+        const token = rand(180, 36)+"_"+req.body.userID;
+
+        const firstQuery = await maria.query('UPDATE users SET token = ? WHERE id = ?', [token, req.body.userID]);
+        const updatedUser = await maria.query('SELECT * FROM users WHERE id = ?', [req.body.userID]);
+
+        res.send(updatedUser[0]);
     });
 
     //revoke a token for a user
     app.delete('/api/authentication/:id', async (req, res) => {
+        const user = await maria.query('SELECT * FROM users WHERE id = ?', [req.params.id]);
+        if(user.length === 0){
+            res.status(404).send(`user with id ${req.params.id} not found`);
+            return;
+        }
 
+        const firstQuery = await maria.query('UPDATE users SET token = null WHERE id = ?', [req.params.id]);
+        const updatedUser = await maria.query('SELECT * FROM users WHERE id = ?', [req.params.id]);
+
+        res.send(updatedUser[0]);
     });
 }  
