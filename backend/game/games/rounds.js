5d9a570363ada6650a37e88e42e00792b53dadc9
Andreas Hanuj <hanuja2@gmail.com>
2019-09-05 18:25:16 +0200

    categories1/2 + update

--- backend/game/games/rounds.js
+++ backend/game/games/rounds.js
@@ -3,9 +3,13 @@
 const authentication = require('../authentication.js');
 const maria = require('../maria.js');
 const joi = require('@hapi/joi');
+var seedrandom = require('seedrandom');
 
 const schemaAnswer = {
     answerID: joi.number().required()
+}
+const schemaCategory = {
+    categoryID: joi.number().required()
 }
 
 module.exports = function(app){
@@ -13,10 +17,12 @@ module.exports = function(app){
     //get current round for existing game
     app.get('/api/rounds', async (req, res) => {
 
+        //check parameter
         if(!req.query.forGame){
             res.status(400).send(`You cant query api/games without giving an additional parameter`);
         }
 
+        //check if game exists
         var game = await maria.query('SELECT * FROM games WHERE id = ?',[req.query.forGame]);
         if(game.length === 0){
             res.status(404).send(`game with id ${req.query.forGame} not found`);
@@ -24,6 +30,7 @@ module.exports = function(app){
         }
         game = game[0]
 
+        //check authentication if player is in round
         const token = req.get("authentication");
         var userID = await authentication.getUserID(token);
         var playerID = 0;
@@ -42,6 +49,7 @@ module.exports = function(app){
             return;
         }
 
+        //get rounds for game
         var rounds = await maria.query('SELECT * FROM rounds WHERE gameID = ?', [req.query.forGame]);
         var openRound;
         for (var i = 0; i < rounds.length; i++) {
@@ -51,15 +59,15 @@ module.exports = function(app){
             }
         }        
 
+        //respond if an opened round exist
         if(openRound){
-            //opened round exist
             var output = serializeRound(openRound);
             output["thisPlayer"] = playerID;
             res.send(output);
             return;
         }
         
-        //no opened round exist
+        //responde if no opened round exist
         if(rounds.length === 6){
             res.status(404).send(`game with id ${req.query.forGame} is over but not marked as over. Something went wrong internally`);
             return;
@@ -67,31 +75,96 @@ module.exports = function(app){
         if(((game['userID_1'] == userID) && ((rounds.length % 2) === 0)) || ((game['userID_2'] == userID) && ((rounds.length % 2) === 1)) ){
             res.status(404).send(`waiting for other play to create a new round`);
             return;
-        }
-      
-        //create new round
-        const questions = await maria.query('SELECT * FROM questions ORDER BY RAND() LIMIT 3');
+        } 
 
-        const firstQuery = await maria.query('INSERT INTO rounds (gameID, questionID_1, questionID_2, questionID_3) VALUES (?, ?, ?, ?); SELECT LAST_INSERT_ID();', [req.query.forGame, questions[0]['id'], questions[1]['id'], questions[2]['id']]);
-        const newRound = await maria.query('SELECT * FROM rounds WHERE id = ?', firstQuery[1][0]["LAST_INSERT_ID()"]);
-        
-        //add here
-        var output = serializeRound(newRound[0]);
-        output["thisPlayer"] = playerID;
-        res.send(output);
+        //TODO att category names
+        var categories = getRandomCategories(req.query.forGame, rounds.length);
+        var response = {};
+        response["gameID"] = req.query.forGame;
+        response["category"]= categories;
+        res.send(response);
         return;
     });  
 
     //answer the current question. Closes round if finished, closes the game if finished
     app.put('/api/rounds/:id', async (req, res) => {
        
-        const sanitizeResult = joi.validate(req.body, schemaAnswer);
+        //check if answer
+        var sanitizeResult = joi.validate(req.body, schemaAnswer);
         if(sanitizeResult.error){
-            res.status(400).send(sanitizeResult.error.details[0].message);
+            var sanitizeResult = joi.validate(req.body, schemaCategory);
+            if(sanitizeResult.error){
+                res.status(400).send(sanitizeResult.error.details[0].message);
+                return;
+            }else{
+                await handleCategory(req, res)
+            }
+        }else{
+            await handleAnswer(req, res)
+        }        
+    });  
+
+    
+    //get specific game
+    app.get('/api/rounds/:id', async (req, res) => {
+        const token = req.get("authentication");
+        var isAdmin = await authentication.isAdmin(token);
+        var userID = await authentication.getUserID(token);
+
+        const rounds = await maria.query('SELECT * FROM rounds WHERE id = ?', req.params.id);
+        if(rounds.length === 0){
+            if(!isAdmin){
+                res.status(401).send("You did not provide an authorized admin token or a token of a user of this round, with your request")
+                return;
+            }
+            res.status(404).send(`round with id ${req.params.id} not found`);
             return;
         }
 
-        var rounds = await maria.query('SELECT * FROM rounds WHERE id = ?',[req.params.id]);
+        var games = await maria.query('SELECT * FROM games WHERE id = ?', rounds[0]["gameID"]);
+
+        var isAuthorized = isAdmin || (games.length > 0 && (games[0]["userID_1"] === userID || games[0]["userID_1"] === userID))
+        if(!isAuthorized){
+            res.status(401).send("You did not provide an authorized admin token or a token of a user of this game, with your request")
+            return;
+        }
+
+        res.send(serializeRound(rounds[0]));    
+    });
+}  
+
+function serializeRound(round) {
+    var newRound = {};
+    newRound["id"] = round["id"];
+    newRound["gameID"] = round["gameID"];
+    newRound["category"] = round["category"];
+
+    var q = [];
+
+    for(var i=1; i<=3; i++){
+        q[i-1] = {};
+        q[i-1]["questionID"] = round["questionID_"+i];
+        q[i-1]["answerID_1"] = round["answerID_1_"+i];
+        q[i-1]["answerID_2"] = round["answerID_2_"+i];
+    }
+    newRound["questions"] = q;
+    return newRound;
+}
+
+function getRandomCategories(gameID, roundNumber){
+    var rng = seedrandom(gameID, roundNumber);
+    var rounds = [];
+    while(rounds.length<3){
+        //TODO validate interval
+        var i = parseInt(rng()*5);
+        if(!rounds.includes(i))
+            rounds.push(i);
+    }
+    return rounds;
+}
+
+async function handleAnswer(req, res){
+    var rounds = await maria.query('SELECT * FROM rounds WHERE id = ?',[req.params.id]);
         if(rounds.length === 0){
             res.status(401).send("Something went wrong with verifying your token. The round you ask may not exist or you have no valid token for the round.")
             return;            
@@ -159,52 +232,71 @@ module.exports = function(app){
             }
         }
         res.status(404).send('The question you are attempting to answer is closed. Waiting for other player to finish round.');
-    });  
-
-    
-    //get specific game
-    app.get('/api/rounds/:id', async (req, res) => {
-        const token = req.get("authentication");
-        var isAdmin = await authentication.isAdmin(token);
-        var userID = await authentication.getUserID(token);
-
-        const rounds = await maria.query('SELECT * FROM rounds WHERE id = ?', req.params.id);
-        if(rounds.length === 0){
-            if(!isAdmin){
-                res.status(401).send("You did not provide an authorized admin token or a token of a user of this round, with your request")
-                return;
-            }
-            res.status(404).send(`round with id ${req.params.id} not found`);
-            return;
-        }
-
-        var games = await maria.query('SELECT * FROM games WHERE id = ?', rounds[0]["gameID"]);
-
-        var isAuthorized = isAdmin || (games.length > 0 && (games[0]["userID_1"] === userID || games[0]["userID_1"] === userID))
-        if(!isAuthorized){
-            res.status(401).send("You did not provide an authorized admin token or a token of a user of this game, with your request")
-            return;
-        }
-
-        res.send(serializeRound(rounds[0]));    
-    });
-}  
-
-function serializeRound(round) {
-    var newRound = {};
-    newRound["id"] = round["id"];
-    newRound["gameID"] = round["gameID"];
-    newRound["category"] = round["category"];
-
-    var q = [];
-
-    for(var i=1; i<=3; i++){
-        q[i-1] = {};
-        q[i-1]["questionID"] = round["questionID_"+i];
-        q[i-1]["answerID_1"] = round["answerID_1_"+i];
-        q[i-1]["answerID_2"] = round["answerID_2_"+i];
+}
+async function handleCategory(req, res){
+        
+    //check if game exists
+    var game = await maria.query('SELECT * FROM games WHERE id = ?',[req.params.id]);
+    if(game.length === 0){
+        res.status(404).send(`game with id ${req.params.id} not found`);
+        return;
     }
-    newRound["questions"] = q;
-    console.log(round)
-    return newRound;
+    game = game[0]
+        
+    //check authentication if player is in round
+    const token = req.get("authentication");
+    var userID = await authentication.getUserID(token);
+    var playerID = 0;
+    var isAuthorized = false;
+    if(game["userID_1"] === userID){
+        playerID = 1;
+        isAuthorized = true;
+    } 
+    if(game["userID_2"] === userID){
+        playerID = 2;
+        isAuthorized = true;
+    }         
+    
+    if(!isAuthorized){
+        res.status(401).send("Your provided token is not valid for a user of the game.")
+        return;
+    }
+        
+       //get rounds for game
+       var rounds = await maria.query('SELECT * FROM rounds WHERE gameID = ?', [req.params.id]); 
+       var openRound;
+       for (var i = 0; i < rounds.length; i++) {
+           if(!rounds[i]['answerID_1_3'] || !rounds[i]['answerID_2_3']){
+               openRound = rounds[i];
+               break;
+           }
+       }        
+        
+        //respond if an opened round exist
+        if(openRound || rounds.length === 6 ){
+            res.status(404).send(`It is not expected that you choose a category currently!`);
+            return;
+        }
+        if(((game['userID_1'] == userID) && ((rounds.length % 2) === 0)) || ((game['userID_2'] == userID) && ((rounds.length % 2) === 1)) ){
+            res.status(404).send(`waiting for other play to choose a category`);
+            return;
+        } 
+
+        var categories = getRandomCategories(req.params.id, rounds.length);
+        if(!categories.includes(req.body.categoryID)){
+            res.status(404).send(`You tried select an invalid category`);
+            return;
+        }      
+        
+        //TODO where category matches
+        //creates new round
+        const questions = await maria.query('SELECT * FROM questions ORDER BY RAND() LIMIT 3');
+        const firstQuery = await maria.query('INSERT INTO rounds (gameID, category, questionID_1, questionID_2, questionID_3) VALUES (?, ?, ?, ?, ?); SELECT LAST_INSERT_ID();', [req.params.id, parseInt(req.body.categoryID) ,questions[0]['id'], questions[1]['id'], questions[2]['id']]);
+        const newRound = await maria.query('SELECT * FROM rounds WHERE id = ?', firstQuery[1][0]["LAST_INSERT_ID()"]);
+                        
+        //respond new round
+        var output = serializeRound(newRound[0]);
+        output["thisPlayer"] = playerID;
+        res.send(output);
+        return;
 }
